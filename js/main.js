7311cd98c2f63a5b8ce940c7876e1032d549331c
Nelson David Daza <yankeenelson@gmail.com>
2021-11-16 21:32:39 -0800

    Task 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 var $searchInput = document.querySelector('.search-input');
 var $searchForm = document.querySelector('.search-form');
+var $viewSelectorAll = document.querySelectorAll('.view');
 
 function userSearch(event) {
   event.preventDefault();
@@ -11,6 +12,26 @@ function userSearch(event) {
   });
   xhr.send();
   $searchForm.reset();
+  switchView('search-bar-result');
 }
 
 $searchForm.addEventListener('submit', userSearch);
+
+/// // VIEW NAVIGATION /////
+
+function switchView(viewName) {
+
+  for (var i = 0; i < $viewSelectorAll.length; i++) {
+    if ($viewSelectorAll[i].getAttribute('data-view') === viewName) {
+      $viewSelectorAll[i].className = 'view';
+    } else {
+      $viewSelectorAll[i].className = 'view hidden';
+    }
+  }
+}
+
+function handleViewNavigation(event) {
+  $searchForm.reset();
+  var buttonDataView = event.target.getAttribute('data-view');
+  switchView(buttonDataView);
+}
