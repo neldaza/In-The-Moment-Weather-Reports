bffe2c059bbea717fd65d0962e011ee20e28c1d8
Nelson David Daza <yankeenelson@gmail.com>
2021-11-18 19:14:43 +0000

    Tasks 1-6

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 /* eslint-disable no-unused-vars */
 var $searchInput = document.querySelector('.search-input');
 var $searchForm = document.querySelector('.search-form');
-var $viewSelectorAll = document.querySelectorAll('.view');
+var $cityResultName = document.querySelector('.city-name');
+var $cityResultTime = document.querySelector('.city-time');
+var $weatherStatusResult = document.querySelector('.weather-status');
+var $fahrenheitResult = document.querySelector('.fahrenheit-td');
+var $humidityResult = document.querySelector('.humidity-td');
+var $gustResult = document.querySelector('.gust-td');
+var $cloudCoverageResult = document.querySelector('.cloud-cover-td');
+var $feelsLikeResult = document.querySelector('.feels-like-td');
+var $windMphResult = document.querySelector('.wind-mph-td');
+var $precipitationResult = document.querySelector('.precipitation-td');
+var $visibilityResult = document.querySelector('.visibility-td');
 
 function userSearch(event) {
   event.preventDefault();
@@ -9,6 +19,37 @@ function userSearch(event) {
   xhr.open('GET', 'http://api.weatherapi.com/v1/current.json?key=182e266b7561494e81d230926211611&q=' + $searchInput.value + '&aqi=no');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
+    var XHRLocalTime = xhr.response.location.localtime;
+    var timeAndDateString = '';
+    timeAndDateString += '' + XHRLocalTime + '';
+    var timeAndDateArray = [];
+    var currentTimeOrDate = '';
+
+    for (var a = 0; a < timeAndDateString.length; a++) {
+      if (timeAndDateString[a] === ' ') {
+        timeAndDateArray.push(currentTimeOrDate);
+        currentTimeOrDate = '';
+      } else {
+        currentTimeOrDate += timeAndDateString[a];
+      }
+
+    }
+    timeAndDateArray.push(currentTimeOrDate);
+
+    var weatherStatus = Object.values(xhr.response.current.condition)[0];
+
+    $cityResultName.textContent = xhr.response.location.name + ', ' + xhr.response.location.region;
+    $cityResultTime.textContent = timeAndDateArray[1] + ' UTC Local Time';
+    $weatherStatusResult.textContent = weatherStatus;
+    $fahrenheitResult.textContent = xhr.response.current.temp_f + '\xB0' + ' ' + 'Fahrenheit';
+    $feelsLikeResult.textContent = 'Feels like ' + xhr.response.current.feelslike_f + '\xB0' + ' Fahreinheit';
+    $gustResult.textContent = xhr.response.current.gust_mph + 'mph' + ' Gust';
+    $cloudCoverageResult.textContent = xhr.response.current.cloud + '%' + ' Cloud Coverage';
+    $visibilityResult.textContent = xhr.response.current.vis_miles + ' Miles Visibility';
+    $windMphResult.textContent = xhr.response.current.wind_mph + 'mph' + ' Wind';
+    $precipitationResult.textContent = xhr.response.current.precip_in + 'in' + ' Precipitation';
+    $humidityResult.textContent = xhr.response.current.humidity + '%' + ' Humidity';
+
   });
   xhr.send();
   $searchForm.reset();
@@ -18,6 +59,7 @@ function userSearch(event) {
 $searchForm.addEventListener('submit', userSearch);
 
 /// // VIEW NAVIGATION /////
+var $viewSelectorAll = document.querySelectorAll('.view');
 
 function switchView(viewName) {
 
