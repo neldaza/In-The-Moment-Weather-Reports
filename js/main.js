0a3e5d508ea809ebb737def0a19ecceef8fd1e1d
Nelson <yankeenelson@gmail.com>
2021-11-24 00:21:46 +0000

    WIP

--- js/main.js
+++ js/main.js
@@ -77,6 +77,21 @@ function submitFunction(event) {
   switchView('search-bar');
 }
 
+function initialDOMNewSubmissionRender(entry) {
+  var dataViewDiv = document.createElement('div');
+  var containerDiv = document.createElement('div');
+  var rowDiv = document.createElement('div');
+  var columnFullDiv = document.createElement('div');
+  var h1ListHeading = document.createElement('h1');
+  var ul = document.createElement('ul');
+
+  dataViewDiv.setAttribute('data-view', '' + $cityResultName.textContent + '');
+  dataViewDiv.setAttribute('class', 'view hidden');
+  containerDiv.setAttribute('class', 'container');
+  rowDiv.setAttribute('class', 'row');
+  columnFullDiv.setAttribute('class', 'column-full text-align-center');
+}
+
 $photoUrl.addEventListener('input', srcUpdate);
 $form.addEventListener('submit', submitFunction);
 $searchForm.addEventListener('submit', userSearch);
