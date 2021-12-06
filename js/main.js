416448c2fb9c97e258152829e8c8a34262e3040e
Nelson <yankeenelson@gmail.com>
2021-12-06 03:44:11 +0000

    WIP

--- js/main.js
+++ js/main.js
@@ -139,6 +139,30 @@ function reportsPageRenderForLoop(cityName) {
 
   return li;
 }
+function DOMDataViewForLoop(entry) {
+  var dataViewDiv = document.createElement('div');
+  var containerDiv = document.createElement('div');
+  var rowDiv = document.createElement('div');
+  var columnFullDiv = document.createElement('div');
+  var h1ListHeading = document.createElement('h1');
+  var cityNameUl = document.createElement('ul');
+  var h1ListHeadingTextContent = document.createTextNode($cityResultName.textContent);
+
+  dataViewDiv.setAttribute('data-view', $cityResultName.textContent);
+  dataViewDiv.setAttribute('class', 'view hidden');
+  containerDiv.setAttribute('class', 'container');
+  rowDiv.setAttribute('class', 'row');
+  columnFullDiv.setAttribute('class', 'column-full text-align-center');
+  h1ListHeading.appendChild(h1ListHeadingTextContent);
+  cityNameUl.setAttribute('class', $cityResultName.textContent);
+
+  columnFullDiv.append(h1ListHeading, cityNameUl);
+  rowDiv.append(columnFullDiv);
+  containerDiv.append(rowDiv);
+  dataViewDiv.append(containerDiv);
+
+  return dataViewDiv;
+}
 
 function mainHTMLDOMNewDataViewSubmissionRender(entry) {
   var dataViewDiv = document.createElement('div');
@@ -147,15 +171,22 @@ function mainHTMLDOMNewDataViewSubmissionRender(entry) {
   var columnFullDiv = document.createElement('div');
   var h1ListHeading = document.createElement('h1');
   var cityNameUl = document.createElement('ul');
+  var h1ListHeadingTextContent = document.createTextNode($cityResultName.textContent);
 
   dataViewDiv.setAttribute('data-view', $cityResultName.textContent);
   dataViewDiv.setAttribute('class', 'view hidden');
   containerDiv.setAttribute('class', 'container');
   rowDiv.setAttribute('class', 'row');
   columnFullDiv.setAttribute('class', 'column-full text-align-center');
-  h1ListHeading.textContent = $cityResultName.textContent;
+  h1ListHeading.appendChild(h1ListHeadingTextContent);
   cityNameUl.setAttribute('class', $cityResultName.textContent);
 
+  columnFullDiv.append(h1ListHeading, cityNameUl);
+  rowDiv.append(columnFullDiv);
+  containerDiv.append(rowDiv);
+  dataViewDiv.append(containerDiv);
+
+  return dataViewDiv;
 }
 
 function mainHTMLDOMNewListSubmissionRender(entry) {
@@ -236,6 +267,7 @@ function mainHTMLDOMNewListSubmissionRender(entry) {
 for (var a = 0; a < data.cities.length; a++) {
   var city = data.cities[a];
   $citiesReportsListUl.append(reportsPageRenderForLoop(city));
+  mainElement.append();
 }
 
 /// /////
