318f67e1723ab00981f2d45f5ea20863df0c7dc9
Nelson David Daza <yankeenelson@gmail.com>
2022-03-24 15:13:30 -0400

    HTML, and CSS Up to par with Figma on mobile

--- js/bundle/bundle.js
+++ js/bundle/bundle.js
@@ -609,16 +609,21 @@ module.exports = switchView;
 
 },{}],15:[function(require,module,exports){
 var $imageInput = document.querySelector('.image-url-holder');
+var $placeholderImage = document.querySelector('.placeholder-img');
 
 function showImageInput(event) {
-  if ($imageInput.className === 'image-url-holder width-80p margin-auto hidden') {
+  if ($imageInput.className === 'image-url-holder width-80p margin-auto hidden' &&
+    $placeholderImage.className === 'placeholder-img hidden') {
     $imageInput.className = 'image-url-holder width-80p margin-auto view';
+    $placeholderImage.className = 'placeholder-img view';
   }
 }
 
 function hideImageInput(event) {
-  if ($imageInput.className === 'image-url-holder width-80p margin-auto view') {
+  if ($imageInput.className === 'image-url-holder width-80p margin-auto view' &&
+    $placeholderImage.className === 'placeholder-img view') {
     $imageInput.className = 'image-url-holder width-80p margin-auto hidden';
+    $placeholderImage.className = 'placeholder-img hidden';
   }
 }
 
