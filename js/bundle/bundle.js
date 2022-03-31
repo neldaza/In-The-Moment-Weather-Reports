d6b2652b87ded9b596f0611b73469c9c4e08c3c3
Nelson David Daza <yankeenelson@gmail.com>
2022-03-31 17:13:34 -0400

    Possible future CSS Implementation, commented out to provide consistency throughout window sizes

--- js/bundle/bundle.js
+++ js/bundle/bundle.js
@@ -712,15 +712,19 @@ module.exports = switchView;
 var $imageInput = document.querySelector('.image-url-holder');
 var $placeholderImage = document.querySelector('.placeholder-img');
 var $formInputsHolder = document.querySelector('.form-inputs-holder');
+var $formSubmitHolder = document.querySelector('.form-submit-holder');
+var $saveButtonHolder = document.querySelector('.save-button-holder');
 
 function showImageInput(event) {
   if ($imageInput.className === 'image-url-holder width-80p margin-auto hidden' &&
     $placeholderImage.className === 'placeholder-img hidden') {
     $imageInput.className = 'image-url-holder width-80p margin-auto view';
     $placeholderImage.className = 'placeholder-img view';
-    if (window.innerWidth >= 1280) {
-      $formInputsHolder.className = 'form-inputs-holder-image';
-    }
+    // if (window.innerWidth >= 1280) {
+    //   $formInputsHolder.className = 'form-inputs-holder-image width-inherit';
+    //   $formSubmitHolder.className = 'form-submit-holder-1280 column-half text-align-right margin-auto';
+    //   $saveButtonHolder.className = 'save-button-holder-1280 row width-inherit';
+    // }
   }
 }
 
@@ -729,8 +733,9 @@ function hideImageInput(event) {
     $placeholderImage.className === 'placeholder-img view') {
     $imageInput.className = 'image-url-holder width-80p margin-auto hidden';
     $placeholderImage.className = 'placeholder-img hidden';
-    $formInputsHolder.className = 'form-inputs-holder';
-
+    $formInputsHolder.className = 'form-inputs-holder width-inherit';
+    $formSubmitHolder.className = 'form-submit-holder column-half text-align-right margin-auto';
+    $saveButtonHolder.className = 'save-button-holder row width-inherit';
   }
 }
 
